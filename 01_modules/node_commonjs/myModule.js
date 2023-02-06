319592c527d89ee9b4df160c97e8659901eb5a8f
Alexander <alex030tstud.kea.dk>
2023-02-06 11:57:23 +0100

    Examaples of imports in node

---
+++ 01_modules/node_commonjs/myModule.js
@@ -0,0 +1,13 @@
+class ClassA{
+    constructor(){
+        console.log("ClassA");
+    }
+}
+
+class ClassB{
+    constructor(){
+        console.log("ClassB");
+    }
+}
+
+module.exports = ClassA
---
+++ 01_modules/node_esmodules/myModule.js
@@ -0,0 +1,13 @@
+class ClassA{
+    constructor(){
+        console.log("ClassA");
+    }
+}
+
+class ClassB{
+    constructor(){
+        console.log("ClassB");
+    }
+}
+
+export default ClassA;
