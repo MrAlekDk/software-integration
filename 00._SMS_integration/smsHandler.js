8e13d5746718840e62b9617432412d595fbc0ab6
Alexander <alex030tstud.kea.dk>
2023-06-12 17:11:44 +0200

    Got a new auth token because earlier one was leaked

--- 00._SMS_integration/smsHandler.js
+++ 00._SMS_integration/smsHandler.js
@@ -7,7 +7,7 @@ import "dotenv/config";
 // Set environment variables for your credentials
 // Read more at http://twil.io/secure
 const accountSid = "AC42421619408ec41c27bcba291dc5558c";
-const authToken = "98da679eda320c0c1487ce87ee2e5ed3";
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 export default client;
